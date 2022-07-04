535db86928d66a76743c95858b29a9c5f6514716
RomaricKc1 <45500198+RomaricKc1@users.noreply.github.com>
2022-07-04 04:35:22 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use std::env;
 use std::process;
 
-use alu::Instruction;
+use alu::{/*Instruction,*/ Core};
+
 
 fn main() {
-    println!("AVR-8 I simulatior");
+    println!("AVR-like Instruction Set simulator");
 
     let args: Vec<String> = env::args().collect();
 
-    let addr_mode = Instruction::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing args, {}", err);
-        process::exit(1);
-    });
+    if args.len() < 2{
+        panic!("Error, \nname binary_file_path");
+    }
+    let file_path = args[1].clone();
+    let mut core = Core::new();
+    
+    core.init_system(String::from(file_path));
 
-    if let Err(e) = Instruction::fetch(addr_mode){
-        println!("Unexpected error lol {}", e);
+
+    if let Err(e) = core.run_program(){
+        println!("Unexpected error: {}", e);
         process::exit(1);
     }
+    core.dump_memory();
 
 }
